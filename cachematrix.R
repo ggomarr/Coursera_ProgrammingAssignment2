85ca1064a51f075f39bbceaaf205f0a933870268
Gonzalo <ggomarr@gmail.com>
2015-02-15 20:48:51 +0800

    Modified cachematrix.R according to the assignment instructions in README.md

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions define an 'ubermatrix' object that allows the inverse of a matrix to be cached
+## for quick recall.
 
-## Write a short comment describing this function
+## makeCacheMatrix takes a matrix and initializes the 'ubermatrix' object (which contains the
+## matrix, a placeholder for its inverse, and functions to change and set both the matrix and its
+## inverse).
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(cache_inv) inv <<- cache_inv
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve returns the inverse of the 'ubermatrix' defined above. If the inverse has not been
+## requested before, it calculates it and stores it in the allocated space within the 'ubermatrix'
+## object, and if it has been requested before it just retrieves it.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if(!is.null(inv)) {
+      message("getting cached data")
+      return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinv(inv)
+    inv
 }
